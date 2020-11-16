5429ccdfa6914e4b695bd7ffcfb3ac0a95a7ca3f
Daniel <mabadejedaniel1@gmail.com>
2020-11-16 11:48:08 -0800

    added url_helper

--- app/controllers/Forms.php
+++ app/controllers/Forms.php
@@ -10,11 +10,16 @@ class Forms extends Controller
     {
         $this->form = new Sample_Form_Creator();
         $this->formModel=$this->model('Form');
+        $this->user=isLoggedIn();
     }
 
     public function add($var = null)
     {
-        $uniqueId=generateUniqueId();
+        if (isLoggedIn() && $this->user) {
+            $uniqueId=generateUniqueId();
+        } else {
+            # code...
+        }
     }
     public function edit($var)
     {
