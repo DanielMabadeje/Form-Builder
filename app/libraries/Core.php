10a823eaac05ad92c3086395796d525baea8fb50
Daniel <mabadejedaniel1@gmail.com>
2020-11-18 11:32:57 -0800

    edited core.php

--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -12,34 +12,70 @@ class Core
     protected $param = [];
     public function __construct()
     {
-        // print_r($this->geturl());
         $url = $this->geturl();
-
-        //Look for controllers
-        if (file_exists('../app/controllers/' . ucwords($url[0]) . '.php')) {
-            $this->currentController = ucwords($url[0]);
-            //unset 0  index
-            unset($url[0]);
-        }
-
-        //require controllers
-        require_once '../app/controllers/' . $this->currentController . '.php';
-        $this->currentController = new $this->currentController;
-
-        //check
-        if (isset($url[1])) {
-            //check if method exists in controller
-            if (method_exists($this->currentController, $url[1])) {
-                $this->currentMethod=$url[1];
+        if ($url[0] == 'api') {
+            // va
+            if (file_exists('../app/controllers/api/' . ucwords($url[1]) . '.php')) {
+                $this->currentController = ucwords($url[1]);
+                //unset 0  index
                 unset($url[1]);
             }
+            //require controllers
+            require_once '../app/controllers/api/' . $this->currentController . '.php';
+            $this->currentController = new $this->currentController;
 
+            //check
+            if (isset($url[2])) {
+                //check if method exists in controller
+                if (method_exists($this->currentController, $url[2])) {
+                    $this->currentMethod = $url[2];
+                    unset($url[2]);
+                }
+            }
+            // Get Params
+            $this->params = $url ? array_values($url) : [];
+            // var_dump($url);
+            // die;
+            // call a callback with array of params
+            return call_user_func_array([
+                $this->currentController,
+                $this->currentMethod
+            ], $this->params);
+        } else {
+            //Look for controllers
+        //    if(isset($url[0])){
+            if (file_exists('../app/controllers/' . ucwords($url[0]) . '.php')) {
+                $this->currentController = ucwords($url[0]);
+                //unset 0  index
+                unset($url[0]);
+            }
+        //    }
+
+            //require controllers
+            require_once '../app/controllers/' . $this->currentController . '.php';
+            $this->currentController = new $this->currentController;
+
+            //check
+            if (isset($url[1])) {
+                //check if method exists in controller
+                if (method_exists($this->currentController, $url[1])) {
+                    $this->currentMethod = $url[1];
+                    unset($url[1]);
+                }else{
+                    // $this->currentcontroller->view('pages/404');
+                    
+                        redirect('/pages/page404');
+                    
+                }
+            }
+            // Get Params
+            $this->params = $url ? array_values($url) : [];
+            // call a callback with array of params
+            return call_user_func_array([
+                $this->currentController,
+                $this->currentMethod
+            ], $this->params);
         }
-        // Get Params
-        $this->params=$url ? array_values($url):[];
-         // call a callback with array of params
-         call_user_func_array([$this->currentController,
-         $this->currentMethod], $this->params);
     }
 
     public function geturl()
@@ -48,6 +84,8 @@ class Core
             $url = rtrim($_GET['url'], '/');
             $url = filter_var($url, FILTER_SANITIZE_URL);
             $url = explode('/', $url);
+            // var_dump($url);
+            // die;
             return $url;
         }
     }
