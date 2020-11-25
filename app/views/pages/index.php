1f1b85bbfe268d9dc1f75fc9bb80356849e1bf28
Daniel <mabadejedaniel1@gmail.com>
2020-11-25 13:09:38 -0800

    working on second section

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -1,15 +1,21 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
-<div style="text-align:center; margin-top:14%;">
-    <h1><?php echo $data['title'] ?></h1>
-    <div class="position-relative overflow-hidden p-5 m-0  text-left bg-light landing-bg pt-0 mt-md-0">
-        <div class="col-md-5 p-lg-5 my-5 text-white">
-            <h1 class="display-4 font-weight-normal text-main-color">CITI<strong>HUB</strong></h1>
-            <h4>NIGERIA ENTERPRISE</h4>
+<div style="text-align:center;">
+    <!-- <h1><?php echo $data['title'] ?></h1> -->
+    <div class="position-relative overflow-hidden p-5 m-0  text-center bg-white landing-bg pt-0 mt-md-0">
+        <div class="col-md-7 p-lg-5 my-5 text-dark mx-auto">
+            <h5 class=" text-main-color">Data Collection is key</h5>
             <p class="lead font-weight-normal">And an even wittier subheading to boot. </p>
+            <h1 class="display-4 font-weight-normal">ONLINE FORM BUILDER</h1>
             <a class="btn btn-outline-secondary md-opjjpmhoiojifppkkcdabiobhakljdgm_doc" href="#">Get Started</a>
         </div>
         <div class="product-device shadow-sm d-none d-md-block"></div>
         <div class="product-device product-device-2 shadow-sm d-none d-md-block"></div>
+    </div>
+
+
+    <div class="col-8 mx-auto">
+        <h4>Collect Data of Any Size</h4>
+        <h2 class="display-4 font-weight-normal">We can handle a lot of date <br> before you know it</h2>
     </div>
 
 
