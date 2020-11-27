c548139f496e8ef24a6896e2de0898e3b9cb50c5
Daniel <mabadejedaniel1@gmail.com>
2020-11-27 02:31:53 -0800

    ading new nav link for Blog

--- app/views/inc/navbar.php
+++ app/views/inc/navbar.php
@@ -20,7 +20,7 @@
                     <a class="nav-link text-dark" href="<?= URLROOT ?>pages/about">Features</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-dark" href="<?= URLROOT ?>pages/about">Pricing</a>
+                    <a class="nav-link text-dark" href="<?= URLROOT ?>pages/about">Blog</a>
                 </li>
             </ul>
             <ul class="navbar-nav ml-auto">
