f00ab50b74443209e073381cc4bc1b0145483e19
Daniel <mabadejedaniel1@gmail.com>
2020-11-27 11:13:31 -0800

    added __constructor

--- app/controllers/Blogs.php
+++ app/controllers/Blogs.php
@@ -1,9 +1,14 @@
 <?php
 
 /**
- * Every page loads from view folder
+ * Every page loads from view folder concerning blog
  * in order to load a view inside a folder of the view folder
  * the folder/filename must be parsed
  */
 class Blogs extends Controller
-{}
+{
+    public function __construct(Type $var = null)
+    {
+        # code...
+    }
+}
