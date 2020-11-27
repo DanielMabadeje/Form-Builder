ac60a3c5251b1ab8b34c106356f2dcdf1976c26f
Daniel <mabadejedaniel1@gmail.com>
2020-11-27 11:12:33 -0800

    added Blogs class

---
+++ app/controllers/Blogs.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Every page loads from view folder
+ * in order to load a view inside a folder of the view folder
+ * the folder/filename must be parsed
+ */
+class Blogs extends Controller
+{}
