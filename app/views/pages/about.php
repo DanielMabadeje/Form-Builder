6b1a60978bad7f1f13386a1482e152fddc7f2d7d
Daniel <mabadejedaniel1@gmail.com>
2020-11-27 02:42:02 -0800

    added first section in about.php

--- app/views/pages/about.php
+++ app/views/pages/about.php
@@ -1,6 +1,14 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
 
 
+<div class="position-relative overflow-hidden p-5 m-0  text-center bg-whit landing-bg pt-0 mt-md-0" style="background: linear-gradient(to bottom right, rgb(253, 251, 251), rgba(253, 249, 249, 0)), url('<?= URLROOT; ?>/img/jess-bailey-q10VITrVYUM-unsplash.jpg'); background-size:cover; height:100%;">
+    <div class="col-md-7 p-lg-5 my-5 text-dark mx-auto">
+        <h1 class="display-4 font-weight-normal">About Us</h1>
+    </div>
+    <div class="product-device shadow-sm d-none d-md-block"></div>
+    <div class="product-device product-device-2 shadow-sm d-none d-md-block"></div>
+</div>
+
 <?php require APPROOT . '/views/inc/pagesfooter.php'; ?>
 
 <?php require APPROOT . '/views/inc/footer.php'; ?>
