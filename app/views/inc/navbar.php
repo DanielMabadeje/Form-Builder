d02affaf27db6b33a34a497700f7337f7bad1e6e
Daniel <mabadejedaniel1@gmail.com>
2020-11-27 02:31:21 -0800

    ading new nav link for features

--- app/views/inc/navbar.php
+++ app/views/inc/navbar.php
@@ -16,6 +16,9 @@
                 <li class="nav-item">
                     <a class="nav-link text-dark" href="<?= URLROOT ?>pages/about">Pricing</a>
                 </li>
+                <li class="nav-item">
+                    <a class="nav-link text-dark" href="<?= URLROOT ?>pages/about">Features</a>
+                </li>
             </ul>
             <ul class="navbar-nav ml-auto">
                 <?php if (isset($_SESSION['user_id'])) : ?> <li class="nav-item active">
