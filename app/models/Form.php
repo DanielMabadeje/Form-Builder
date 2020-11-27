023a5dad168440d2ec829c71ac160b995f545bf2
Daniel <mabadejedaniel1@gmail.com>
2020-11-27 12:51:31 -0800

    adding placeholder param

--- app/models/Form.php
+++ app/models/Form.php
@@ -55,7 +55,7 @@ class Form
         $this->db->bind(':label', $data['label']);
         $this->db->bind(':type', $data['type']);
         $this->db->bind(':name', $data['name']);
-        $this->db->bind(':description', $data['placholder']);
+        $this->db->bind(':description', $data['placeholder']);
         
     }
 
