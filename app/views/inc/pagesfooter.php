2635305091081c6605cd00aa869d0e6e011a16be
Daniel <mabadejedaniel1@gmail.com>
2020-11-27 02:38:26 -0800

    moved footer contents to pages footer

---
+++ app/views/inc/pagesfooter.php
@@ -0,0 +1,88 @@
+<div class="footer pt-md-5 pb-0 mb-0 bg-white">
+    <footer class="pt-md-5">
+        <div class="col-md-12">
+            <div class="row">
+                <div class="col-md-4 mb-4">
+                    <h2 class="display-4">
+                        <?= SITENAME; ?>
+                    </h2>
+                    <br>
+                    <div class="socials">
+                        <h4>
+                            <i class="fa fa-facebook"></i>
+                            <i class="fa fa-instagram"></i>
+                            <i class="fa fa-twitter"></i>
+                            <i class="fa fa-linkedin"></i>
+                        </h4>
+                    </div>
+                    <div class="">
+                        <span>Playstore</span>
+                        <span>AppleStore</span>
+                    </div>
+                </div>
+
+                <div class="col">
+                    <h4>Buisness</h4>
+                    <ul class="navbar-nav">
+                        <li class="nav-item mb-2"><a href="" class="text-dark">My Forms</a></li>
+                        <li class="nav-item mb-2"><a href="" class="text-dark">Home</a></li>
+                        <li class="nav-item mb-2"><a href="" class="text-dark">Features</a></li>
+                        <li class="nav-item mb-2"><a href="" class="text-dark">Template</a></li>
+                        <li class="nav-item mb-2"><a href="" class="text-dark">Pricing</a></li>
+                        <li class="nav-item mb-2"><a href="" class="text-dark">Blog</a></li>
+                    </ul>
+                </div>
+
+                <div class="col">
+                    <h4><?= SITENAME; ?></h4>
+                    <ul class="navbar-nav">
+                        <li class="nav-item mb-2"><a href="" class="text-dark">Download</a></li>
+                        <li class="nav-item mb-2"><a href="" class="text-dark">Survey Maker</a></li>
+                        <li class="nav-item mb-2"><a href="" class="text-dark">Design</a></li>
+                        <li class="nav-item mb-2"><a href="" class="text-dark">Integrations</a></li>
+                        <li class="nav-item mb-2"><a href="" class="text-dark">About Us</a></li>
+                        <li class="nav-item mb-2"><a href="" class="text-dark">Login</a></li>
+                        <li class="nav-item mb-2"><a href="" class="text-dark">Register</a></li>
+                    </ul>
+                </div>
+
+                <div class="col">
+                    <h4>Support</h4>
+                    <ul class="navbar-nav">
+                        <li class="nav-item mb-2"><a href="" class="text-dark">Help</a></li>
+                        <li class="nav-item mb-2"><a href="" class="text-dark">Packages</a></li>
+                        <li class="nav-item mb-2"><a href="" class="text-dark">Sponsorship</a></li>
+                        <li class="nav-item mb-2"><a href="" class="text-dark">Contact Us</a></li>
+                        <li class="nav-item mb-2"><a href="" class="text-dark">FAQ</a></li>
+                    </ul>
+                </div>
+
+                <div class="col">
+                    <h4>Compare</h4>
+                    <ul class="navbar-nav">
+                        <li class="nav-item mb-2"><a href="" class="text-dark">Google Forms</a></li>
+                        <li class="nav-item mb-2"><a href="" class="text-dark">TypeForm</a></li>
+                        <li class="nav-item mb-2"><a href="" class="text-dark">Forms App</a></li>
+                        <li class="nav-item mb-2"><a href="" class="text-dark">Survey Monkey</a></li>
+                        <li class="nav-item mb-2"><a href="" class="text-dark">Form Builder</a></li>
+                    </ul>
+                </div>
+
+                <div class="col">
+                    <h4>Legal</h4>
+                    <ul class="navbar-nav">
+                        <li class="nav-item mb-2"><a href="" class="text-dark">Privacy Policy</a></li>
+                        <li class="nav-item mb-2"><a href="" class="text-dark">Terms Of Use</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="copyright text-center bg-light pt-4 pb-4 col-12">
+
+            <h5 class="text-dark">&copy; CopyRight <?php echo date('Y'); ?> All Right Reserved
+            </h5>
+        </div>
+    </footer>
+</div>
