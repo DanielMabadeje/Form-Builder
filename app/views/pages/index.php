ff973c2b7453c0705201f34bbe938d789ec002e2
Daniel <mabadejedaniel1@gmail.com>
2020-11-27 02:28:28 -0800

    working on responsiveness

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -206,7 +206,7 @@
   <footer class="pt-md-5">
     <div class="col-md-12">
       <div class="row">
-        <div class="col-md-4">
+        <div class="col-md-4 mb-4">
           <h2 class="display-4">
             <?= SITENAME; ?>
           </h2>
