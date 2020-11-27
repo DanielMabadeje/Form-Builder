b8559c9cd6952001c6ec8a16c1c34b9c2687b6e9
Daniel <mabadejedaniel1@gmail.com>
2020-11-27 02:08:27 -0800

    adding margin to footer links

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -229,7 +229,7 @@
           <h3>Buisness</h3>
           <ul class="navbar-nav">
             <li class="nav-item mb-2"><a href="" class="text-dark">My Forms</a></li>
-            <li class="nav-item"><a href="" class="text-dark">My Forms</a></li>
+            <li class="nav-item mb-2"><a href="" class="text-dark">My Forms</a></li>
             <li class="nav-item"><a href="" class="text-dark">My Forms</a></li>
             <li class="nav-item"><a href="" class="text-dark">My Forms</a></li>
             <li class="nav-item"><a href="" class="text-dark">My Forms</a></li>
