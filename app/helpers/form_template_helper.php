b45d579e52939c067d1b82a44d260e2d1f052d85
Daniel <mabadejedaniel1@gmail.com>
2020-11-30 12:12:56 -0800

    editing blank form

--- app/helpers/form_template_helper.php
+++ app/helpers/form_template_helper.php
@@ -1,5 +1,19 @@
 <?php
 
 function blankForm(){
-    
+    return array(
+        array(
+            'id' => 'email', // if 'name' param is not given. Then by default 'id' will be considered as 'name'.
+            'placeholder' => 'Email',
+            'label' => 'Email',
+            'name'=>'email',
+            'type'=>'email'
+        ),
+        array(
+            'id' => 'submit',
+            'label' => 'submit',
+            'type' => 'submit',
+            'name' => 'submit'
+        ),
+    );
 }
