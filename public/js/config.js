4f98a32661973cadd3bc8359dd7b774f34018793
Daniel <mabadejedaniel1@gmail.com>
2020-12-03 12:27:12 -0800

    edited config.js

--- public/js/config.js
+++ public/js/config.js
@@ -1,7 +1,7 @@
 var settings = {
     "async": true,
     "crossDomain": true,
-   "url":base_url+"/api/cart/index",
+   "url":base_url+"/api/",
     "method": "GET",
     "data": {
         // "apiKey": "51e3d490e2da412c988237f19087ad66",
