ca7948c8d416f8cda2b1af4e4eb59468d6783d80
Daniel <mabadejedaniel1@gmail.com>
2020-12-03 00:53:09 -0800

    removed some contents from edit.php

--- app/views/forms/edit.php
+++ app/views/forms/edit.php
@@ -8,34 +8,10 @@
         <div class="card border-radius-none border-none col-md-10 text-left p-3">
             <div class="card-title">
                 <div class="row">
-                    <div class="col-6">
+                    <div class="col-12">
                         <h1 contenteditable="true"><?= $data->form_name; ?></h1>
                     </div>
-                    <div class="col">
-                        <span class="save">Last Edit Was at <?= $data->updated_at; ?></span>
-                    </div>
-                    <div class="col">
-                        <div class="d-flex">
-
-                            <div class="pr-3">
-                                <h5 class="text-dark">Allowing Responses</h5>
-                            </div>
-                            <div>
-                                <?php if ($data->allowing_responses == 'no') : ?>
-                                    <label class="switch ml-auto pt-2">
-                                        <input type="checkbox">
-                                        <div class="slider round"></div>
-                                    </label>
-                                <?php else : ?>
-                                    <label class="switch ml-auto pt-2">
-                                        <input type="checkbox" checked>
-                                        <div class="slider round"></div>
-                                    </label>
-                                <?php endif; ?>
-                            </div>
-                        </div>
-
-                    </div>
+                    
                 </div>
 
                 <br>
---
+++ app/views/forms/inc/editheader.php
@@ -0,0 +1,29 @@
+<div class="col-12 bg-white">
+<div class="text-dark">
+<div class="col">
+                        <span class="save">Last Edit Was at <?= $data->updated_at; ?></span>
+                    </div>
+                    <div class="col">
+                        <div class="d-flex">
+
+                            <div class="pr-3">
+                                <h5 class="text-dark">Allowing Responses</h5>
+                            </div>
+                            <div>
+                                <?php if ($data->allowing_responses == 'no') : ?>
+                                    <label class="switch ml-auto pt-2">
+                                        <input type="checkbox">
+                                        <div class="slider round"></div>
+                                    </label>
+                                <?php else : ?>
+                                    <label class="switch ml-auto pt-2">
+                                        <input type="checkbox" checked>
+                                        <div class="slider round"></div>
+                                    </label>
+                                <?php endif; ?>
+                            </div>
+                        </div>
+
+                    </div>
+</div>
+</div>
