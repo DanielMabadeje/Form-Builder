e105a03eef618ced2dc12718667fab09cb2ac1cc
Daniel <mabadejedaniel1@gmail.com>
2020-12-03 15:14:52 -0800

    added updateformquestion

--- app/models/Form.php
+++ app/models/Form.php
@@ -41,6 +41,8 @@ class Form
 
     public function updateForm($data)
     {
+
+        $form = $data['form'];
         $this->db->query('INSERT INTO forms (form_id, user_id, form_name, description, form_type) VALUES(:form_id, :user_id, :form_name, :description, :form_type)');
         $this->db->bind(':form_id', $data['uniqueId']);
         $this->db->bind(':user_id', $data['user_id']);
@@ -73,6 +75,41 @@ class Form
     }
 
     public function createFormQuestion($data)
+    {
+
+
+        $this->db->query('INSERT INTO 
+                            form_questions 
+                            (form_id, label, type, name, placeholder, id)
+                             VALUES(:form_id, :label, :type, :name, :placeholder, :id)');
+
+
+        $this->db->bind(':form_id', $data['uniqueId']);
+        if (isset($data['questions']['label'])) {
+            $this->db->bind(':label', $data['questions']['label']);
+        } else {
+            $this->db->bind(':label', null);
+        }
+        $this->db->bind(':type', $data['questions']['type']);
+        $this->db->bind(':name', $data['questions']['name']);
+        if (isset($data['questions']['placeholder'])) {
+            $this->db->bind(':placeholder', $data['questions']['placeholder']);
+        } else {
+            $this->db->bind(':placeholder', null);
+        }
+        $this->db->bind(':id', $data['questions']['id']);
+
+
+        var_dump($data);
+        if ($this->db->execute()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    public function updateFormQuestion($data)
     {
 
 
