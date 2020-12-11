2346329d04525a6002dce56f2d3a52e57ea1536e
Daniel <mabadejedaniel1@gmail.com>
2020-12-11 12:25:18 -0800

    editing content

--- app/views/forms/responses.php
+++ app/views/forms/responses.php
@@ -28,36 +28,7 @@
             </div>
         </div>
 
-        <div class="col-md-2 height ml-3 border-radius-none border-none p-3 ">
-            <div class="text-right d-flex d-md-block bg-whiite">
-                <!-- <button class="btn btn-secondary border-radius-none"> <i class="fa fa-plus"></i> Add</button> -->
-                <div class="dropdown p-2 col-sm-2 d-block">
-                    <button onclick="myFunction()" class="dropbtn btn border-radius-none"> <i class="fa fa-plus"></i></button>
-                    <div id="myDropdown" class="dropdown-content text-left">
-                        <div onclick="addItemToForm('email')">Email</div>
-                        <div onclick="addItemToForm('shortanswer')">Short Answer</div>
-                        <div onclick="addItemToForm('longanswer')">TextArea</div>
-                        <div onclick="addItemToForm('dropdown')">Dropdown</div>
-                        <div onclick="addItemToForm('date')">Date</div>
-                        <div onclick="addItemToForm('singleoption')">Single Option</div>
-                        <div>MultiChoice Answer</div>
-                        <div>File</div>
-                        <!-- input -->
-                    </div>
-                </div>
-                <div class="p-2 col-sm-2">
-                    <button class="dropbtn btn  border-radius-none"> <i class="fa fa-camera"></i></button>
-                </div>
 
-                <div class="p-2 col-sm-2">
-                    <button class="dropbtn btn  border-radius-none"> <i class="fa fa-video-camera"></i></button>
-                </div>
-
-                <div class="p-2 col-sm-2">
-                    <button class="dropbtn btn  border-radius-none"> <i class="fa fa-credit-card"></i></button>
-                </div>
-            </div>
-        </div>
     </section>
 </div>
 
