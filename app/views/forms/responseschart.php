7998f10563e7974b2b53879bfe220468bc72f47b
Daniel <mabadejedaniel1@gmail.com>
2020-12-11 12:44:09 -0800

    adding contents

---
+++ app/views/forms/responseschart.php
@@ -0,0 +1,41 @@
+<?php require APPROOT . '/views/inc/header.php'; ?>
+<?php require APPROOT . '/views/forms/inc/editheader.php'; ?>
+<link rel="stylesheet" href="<?= URLROOT; ?>/css/form/edit.css">
+<link rel="stylesheet" href="<?= URLROOT; ?>/css/modal.css">
+
+<link rel="stylesheet" href="<?= URLROOT; ?>/css/font-awesome.min.css">
+<div class="container" style="text-align:center; margin-top:10%;">
+
+
+
+
+    <section class="col-md-12 d-md-flex">
+        <div class="card border-radius-none border-none col-md-10 text-left p-3">
+            <div class="card-title">
+                <div class="row">
+                    <div class="col-12">
+                        <h1 contenteditable="true"><?= $data->form_name; ?></h1>
+                    </div>
+
+                </div>
+
+                <br>
+                <h5 contenteditable="true" class="description_h5"><?= $data->description; ?></h5>
+            </div>
+
+            <div class="card-body">
+                <h2>Responses Page</h2>
+            </div>
+        </div>
+
+
+    </section>
+</div>
+
+<script>
+    var formarray = <?php echo json_encode($data); ?>
+</script>
+
+<script src="<?= URLROOT; ?>/js/jquery.js"></script>
+<script src="<?= URLROOT; ?>/js/form/edit.js"></script>
+<?php require APPROOT . '/views/inc/footer.php'; ?>
