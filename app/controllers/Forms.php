65c6dfa886b0f19b47e574c6c07ed5f59a3333e8
Daniel <mabadejedaniel1@gmail.com>
2020-12-15 11:49:50 -0800

    added semicolon"

--- app/controllers/Forms.php
+++ app/controllers/Forms.php
@@ -73,9 +73,9 @@ class Forms extends Controller
     {
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             $answer_id = date('Y-m-d') . $this->generateRandomChars();
-            $this->sanitizePost()
-            $data=$_POST;
-            $data['answer_id']=$answer_id;
+            $this->sanitizePost();
+            $data = $_POST;
+            $data['answer_id'] = $answer_id;
             var_dump($data);
             die;
         } else {
