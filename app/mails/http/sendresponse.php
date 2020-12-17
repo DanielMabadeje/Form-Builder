9844adc3a75b2f2722aec732cc9dbe5fdb5e18f1
Daniel <mabadejedaniel1@gmail.com>
2020-12-17 08:18:23 -0800

    worked on sendresponse.php at view method

--- app/mails/http/sendresponse.php
+++ app/mails/http/sendresponse.php
@@ -2,18 +2,26 @@
 
 class sendresponse extends Mail
 {
-    private $name;
-    private $password;
-    private $email;
-    private $url;
-    private $courier;
+    // private $name;
+    // private $password;
+    // private $email;
+    // private $url;
+    // private $courier;
+
+    private $key = [];
     public function __construct($data = null)
     {
-        $this->name = $data['name'];
-        $this->email = $data['email'];
-        $this->password = $data['password'];
-        $this->url = $data['url'];
-        $this->courier = $data['courier'];
+        // $this->name = $data['name'];
+        // $this->email = $data['email'];
+        // $this->password = $data['password'];
+        // $this->url = $data['url'];
+        // $this->courier = $data['courier'];
+
+        $index = 0;
+        foreach ($data as $key => $value) {
+            $this->key[$index] = $key;
+            $this->$key = $value;
+        }
 
         // if ($func == 'view') {
         //     $this->view();
@@ -23,15 +31,23 @@ class sendresponse extends Mail
     }
     public function view()
     {
-        $data = [
-            'name' => $this->name,
-            'email' => $this->email,
-            'courier_name' => $this->courier,
-            'password' => $this->password,
-            'url' => $this->url,
-            'year' => date('Y')
-        ];
-        $this->viewhtml($data, 'adddriver.mail');
+        // $data = [
+        //     'name' => $this->name,
+        //     'email' => $this->email,
+        //     'courier_name' => $this->courier,
+        //     'password' => $this->password,
+        //     'url' => $this->url,
+        //     'year' => date('Y')
+        // ];
+
+        foreach ($this->key as $key) {
+            $data[$key] = $this->$key;
+        }
+        $data['year'] = date('Y');
+
+        var_dump($data);
+        die;
+        $this->viewhtml($data, 'sendresponse.mail');
     }
     public function send()
     {
