d4a5bced4b2838260aefaf5a482fd97e9f4e2f8b
Daniel <mabadejedaniel1@gmail.com>
2020-12-16 16:37:55 -0800

    aded url for post request for login

--- app/views/users/login.php
+++ app/views/users/login.php
@@ -5,7 +5,7 @@
             <?php flash('register_success'); ?>
             <h2>Login</h2>
             <p>Please fill the form</p>
-            <form action="<?= URLROOT ?>users/login" method="post">
+            <form action="<?= URLROOT ?>/users/login" method="post">
                 <div class="form-group">
                     <label for="email">Email <sup>*</sup></label>
                     <input type="email" name="email" class="form-control form-control-lg <?= (!empty($data['email_err'])) ? 'is-invalid' : ''; ?>" value="<?= $data['email']; ?>">
