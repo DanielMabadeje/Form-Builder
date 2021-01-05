751d384e548e0600593a43c9055ba414c59c6f20
Daniel <mabadejedaniel1@gmail.com>
2021-01-04 20:53:33 -0800

    cleaning code

--- app/config/config.php
+++ app/config/config.php
@@ -7,25 +7,11 @@ define('DB_NAME', 'form_builder');
 //App Root
 define('APPROOT', dirname(dirname(__FILE__)));
 // define('URLROOT', $_ENV['URL_ROOT']);
-// echo $_ENV;
 
-// $url=$_ENV;
-
-// var_dump($url);
-// die;
 
 
 $url = $_SERVER['REQUEST_URI'];
 $host = $_SERVER['HTTP_HOST'];
-// $env = getenv();
-// var_dump($env);
-
-
-// $env = json_encode($env);
-// $url = json_encode($url);
-// var_dump($url);
-// die($url);
-// die($env);
 
 if (isset($_ENV['URL_ROOT'])) {
     define('URLROOT', $_ENV['URL_ROOT']);
