8de44ebfb383d479c915c998847002553aac558c
Daniel <mabadejedaniel1@gmail.com>
2021-01-06 20:32:51 -0800

    added footer.php

---
+++ app/views/dashboard/inc/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/style.css">
+    <title><?php echo SITENAME; ?></title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/bootstrap.css">
+
+    <!-- script config -->
+    <script src="<?php echo URLROOT ?>/js/jquery.js"></script>
+    <script>
+        const base_url = '<?= URLROOT ?>'
+    </script>
+    <script src="<?php echo URLROOT ?>/js/config.js"></script>
+
+
+
+</head>
+
+<body>
+    <div class="containe">
+
+
+    <div class="d-flex">
---
+++ app/views/dashboard/inc/sidenav.php
@@ -0,0 +1,1 @@
+<div class="col-md-3 bg-white"></div>
