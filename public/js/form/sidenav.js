d14129e3e5ae3cf0b652ec6b78d34f51c47519e3
Daniel <mabadejedaniel1@gmail.com>
2021-01-07 17:49:20 -0800

    added sidenav .js

--- public/js/form/sidenav.js
+++ public/js/form/sidenav.js
@@ -1,7 +1,9 @@
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("mySidenav").style.width = "80%";
+    document.getElementById("mySidenav").style.display = "block";
 }
 
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("mySidenav").style.display = "none";
 }
