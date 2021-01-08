f09dd306e89efca6df849ef76292637d3ab04fd4
Daniel <mabadejedaniel1@gmail.com>
2021-01-08 02:29:22 -0800

    adding the edit option feature

--- app/controllers/api/Forms.php
+++ app/controllers/api/Forms.php
@@ -179,4 +179,24 @@ class Forms extends ApiController
             $this->fail($_SERVER['REQUEST_METHOD'] . ' given instead of POST', 405);
         }
     }
+    public function editOption()
+    {
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $this->sanitizePost();
+            // var_dump($_POST);
+            $data = [];
+            $data['form_id'] = $_POST['form_id'];
+            $data['question_id'] = $_POST['question_id'];
+            $data['id'] = $_POST['type'];
+            $data['value'] = $_POST['option'];
+
+            if ($result = $this->formModel->addQuestionOption($data)) {
+                $this->success($result);
+            } else {
+                $this->fail();
+            }
+        } else {
+            $this->fail($_SERVER['REQUEST_METHOD'] . ' given instead of POST', 405);
+        }
+    }
 }
