646d84fab4afd4bb1100f0e9cb0f70de1a31c141
Daniel <mabadejedaniel1@gmail.com>
2021-01-07 21:51:56 -0800

    added content

---
+++ app/views/dashboard/forms.php
@@ -0,0 +1,27 @@
+
+
+<?php require APPROOT . '/views/dashboard/inc/header.php'; ?>
+
+<?php require APPROOT . '/views/dashboard/inc/sidenav.php'; ?>
+
+<div class="cl ml-auto" id="main">
+<?php require APPROOT . '/views/dashboard/inc/topnav.php'; ?>
+<section class="bg-lighter col-12 p-3">
+    <div>
+        <span class="fa fa-chevron-right"></span>Dashboard
+    </div>
+</section>
+    <section class="container pt-3">
+        <!-- <h2 class="display-4">Dashboard</h2> -->
+
+        <div class="container">
+        
+        </div>
+    </section>
+
+    
+</div>
+
+
+
+<?php require APPROOT . '/views/dashboard/inc/footer.php'; ?>
