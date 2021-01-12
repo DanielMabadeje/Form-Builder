9d71aae429488416617869b964ff4c9a649a901a
Daniel <mabadejedaniel1@gmail.com>
2021-01-12 08:06:49 -0800

    edited layout

--- app/views/pages/quicktutorial.php
+++ app/views/pages/quicktutorial.php
@@ -1,11 +1,20 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
-<section class="bg-light">
-<section class="container mt-md-5 pt-md-5">
-    <section class="mt-md-5 pt-md-5 mb-md-5 pb-md-5 col-md-7">
-        <div class=" mb-md-5 pb-md-5 bg-white">
-            <div class="pl-md-5">
+<section class="bg-light mb-5 pb-5 pt-0 mt-0">
+<section class="container mt-md-5 pt-5">
+
+    <div class="row col-md-3 mb-5">
+        <div class="col-10">
+            <h3>Create a form</h3>
+        </div>
+        <div class="col-2">
+            <a href="<?URLROOT; ?>/forms/add" class="btn btn-primary">Create Form</a>
+        </div>    
+    </div>
+    <section class="mt-md-2 pt-5 mb-5  mx-auto bg-white col-md-7" style="box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);">
+        <div class=" mb-md-5  ">
+            <div class="mb-md-5">
                 <section class="banner-area mt-md-5 overflow-hidden position-relative text-dark" id="lay" data-parallax="scroll" data-image-src="" >
-                    <div id="" class="carousel carousel slide mt-md-5" data-ride="carousel">
+                    <div id="" class="carousel carousel slide mt-md-5" data-ride="carouse">
                         <ul class="carousel-indicators">
                             <li data-target="#demo" data-slide-to="0" class="active"></li>
                             <li data-target="#demo" data-slide-to="1"></li>
@@ -16,36 +25,36 @@
                         </ul>
                         <div class="carousel-inner" style="overflow:visibl">
                             <div class="carousel-item active" id="">
-                            <img src="<?= URLROOT ?>/img/booking.png" alt=""  width="100%" height="200px">
-                                <div class="carousel-caption text-dark position-relative">
-                                    <h3>Music</h3>
-                                    <p> The Key To The Soul</p>
+                            <img src="<?= URLROOT ?>/img/booking.png" alt=""  width="100%" height="300px">
+                                <div class="carousel-caption text-dark position-initial mx-auto">
+                                    <h3>Customized Backgrounds</h3>
+                                    <p> You can add custom backgrounds to your forms</p>
                                 </div>
                             </div>
                             <div class="carousel-item">
-                            <img src="<?= URLROOT ?>/img/booking.png" alt=""  width="100%" height="200px">
-                                <div class="carousel-caption text-dark position-relative">
+                            <img src="<?= URLROOT ?>/img/booking.png" alt=""  width="100%" height="300px">
+                                <div class="carousel-caption text-dark position-initial mx-auto">
                                     <h3>Music</h3>
                                     <p>Creation Of Uncomprehended Feelings</p>
                                 </div>
                             </div>
                             <div class="carousel-item">
-                            <img src="<?= URLROOT ?>/img/booking.png" alt=""  width="100%" height="200px">
-                                <div class="carousel-caption text-dark position-relative">
+                            <img src="<?= URLROOT ?>/img/booking.png" alt=""  width="100%" height="300px">
+                                <div class="carousel-caption text-dark position-initial mx-auto">
                                     <h3>Music</h3>
                                     <p>A Sweet Melody</p>
                                 </div>
                             </div>
                             <div class="carousel-item">
-                            <img src="<?= URLROOT ?>/img/booking.png" alt=""  width="100%" height="200px">
-                                <div class="carousel-caption text-dark position-relative">
+                            <img src="<?= URLROOT ?>/img/booking.png" alt=""  width="100%" height="300px">
+                                <div class="carousel-caption text-dark position-initial mx-auto">
                                     <h3>Music</h3>
                                     <p>The Best</p>
                                 </div>
                             </div>
                             <div class="carousel-item">
-                            <img src="<?= URLROOT ?>/img/booking.png" alt=""  width="100%" height="200px">
-                                <div class="carousel-caption text-dark position-relative">
+                            <img src="<?= URLROOT ?>/img/booking.png" alt=""  width="100%" height="300px">
+                                <div class="carousel-caption text-dark position-initial mx-auto">
                                     <h3>Los Angeles</h3>
                                     <p>Its For All Ages</p>
                                 </div>
