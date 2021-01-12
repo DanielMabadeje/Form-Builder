13a65224554b91fb3e5dfb49e56f37f75c190970
Daniel <mabadejedaniel1@gmail.com>
2021-01-11 18:09:49 -0800

    adding carousel

--- app/views/pages/quicktutorial.php
+++ app/views/pages/quicktutorial.php
@@ -1,7 +1,74 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
 
-<section>
-    
+<section class="container">
+    <section class="mt-md-5 pt-md-5 mb-md-5 pb-md-5">
+        <div class="mt-md-5 pt-md-5 mb-md-5 pb-md-5">
+            <div class="mt-md-5 pt-md-5 pl-md-5">
+                <section class="banner-area relative" id="lay" data-parallax="scroll" data-image-src="">
+                    <div id="demo" class="carousel carousel slide" data-ride="carousel">
+                        <ul class="carousel-indicators">
+                            <li data-target="#demo" data-slide-to="0" class="active"></li>
+                            <li data-target="#demo" data-slide-to="1"></li>
+                            <!-- <li data-target="#demo" data-slide-to="2"></li> -->
+                        </ul>
+                        <div class="carousel-inner">
+                            <div class="carousel-item active" id="">
+                                <img src="" alt="Los Angeles" width="100%" height="400px">
+                                <div class="carousel-caption">
+                                    <h3>Music</h3>
+                                    <p> The Key To The Soul</p>
+                                </div>
+                            </div>
+                            <div class="carousel-item">
+                                <img src="" alt="Chicago" width="100%" height="400px">
+                                <div class="carousel-caption">
+                                    <h3>Music</h3>
+                                    <p>Creation Of Uncomprehended Feelings</p>
+                                </div>
+                            </div>
+                            <div class="carousel-item">
+                                <img src="" alt="New York" width="100%" height="400px">
+                                <div class="carousel-caption">
+                                    <h3>Music</h3>
+                                    <p>A Sweet Melody</p>
+                                </div>
+                            </div>
+                            <div class="carousel-item">
+                                <img src="" alt="New York" width="100%" height="400px">
+                                <div class="carousel-caption">
+                                    <h3>Music</h3>
+                                    <p>The Best</p>
+                                </div>
+                            </div>
+                            <div class="carousel-item">
+                                <img src="" alt="New York" width="100%" height="400px">
+                                <div class="carousel-caption">
+                                    <h3>Los Angeles</h3>
+                                    <p>Its For All Ages</p>
+                                </div>
+                            </div>
+                            <a class="carousel-control-prev" href="#demo" data-slide="prev">
+                                <span class="carousel-control-prev-icon"></span>
+                            </a>
+                            <a class="carousel-control-next" href="#demo" data-slide="next">
+                                <span class="carousel-control-next-icon"></span>
+                            </a>
+                        </div>
+                        <div class="overlay-bg overlay"></div>
+                        <div class="container">
+                            <div class="row-fullscreen">
+                                <div class="banner-content d-flex align-items-center col-lg-12 col-md-12" style="color:gold; height:40px;">
+                                    <h1 id="topic">
+                                        First Music Records<br>
+                                        <p id="sub">
+                                            It's Better Than Ever.</p>
+                                    </h1>
+                                </div>
+                            </div>
+                </section>
+            </div>
+        </div>
+    </section>
 </section>
 
 
