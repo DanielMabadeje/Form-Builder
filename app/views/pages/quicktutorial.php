15ee88f938acf212f457cfb4074a28d7e84c0f77
Daniel <mabadejedaniel1@gmail.com>
2021-01-11 18:59:33 -0800

    eiting carousel

--- app/views/pages/quicktutorial.php
+++ app/views/pages/quicktutorial.php
@@ -1,65 +1,70 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
-
-<section class="container">
-    <section class="mt-md-5 pt-md-5 mb-md-5 pb-md-5">
-        <div class="mt-md-5 pt-md-5 mb-md-5 pb-md-5">
-            <div class="mt-md-5 pt-md-5 pl-md-5">
-                <section class="banner-area relative" id="lay" data-parallax="scroll" data-image-src="">
-                    <div id="demo" class="carousel carousel slide" data-ride="carousel">
+<section class="bg-light">
+<section class="container mt-md-5 pt-md-5">
+    <section class="mt-md-5 pt-md-5 mb-md-5 pb-md-5 col-md-7">
+        <div class=" mb-md-5 pb-md-5 bg-white">
+            <div class="pl-md-5">
+                <section class="banner-area mt-md-5 overflow-hidden position-relative text-dark" id="lay" data-parallax="scroll" data-image-src="" >
+                    <div id="" class="carousel carousel slide mt-md-5" data-ride="carousel">
                         <ul class="carousel-indicators">
                             <li data-target="#demo" data-slide-to="0" class="active"></li>
                             <li data-target="#demo" data-slide-to="1"></li>
+                            <li data-target="#demo" data-slide-to="2"></li>
+                            <li data-target="#demo" data-slide-to="3"></li>
+                            <li data-target="#demo" data-slide-to="4"></li>
                             <!-- <li data-target="#demo" data-slide-to="2"></li> -->
                         </ul>
-                        <div class="carousel-inner">
+                        <div class="carousel-inner" style="overflow:visibl">
                             <div class="carousel-item active" id="">
-                                <img src="" alt="Los Angeles" width="100%" height="400px">
-                                <div class="carousel-caption">
+                            <img src="<?= URLROOT ?>/img/booking.png" alt=""  width="100%" height="200px">
+                                <div class="carousel-caption text-dark position-relative">
                                     <h3>Music</h3>
                                     <p> The Key To The Soul</p>
                                 </div>
                             </div>
                             <div class="carousel-item">
-                                <img src="" alt="Chicago" width="100%" height="400px">
-                                <div class="carousel-caption">
+                            <img src="<?= URLROOT ?>/img/booking.png" alt=""  width="100%" height="200px">
+                                <div class="carousel-caption text-dark position-relative">
                                     <h3>Music</h3>
                                     <p>Creation Of Uncomprehended Feelings</p>
                                 </div>
                             </div>
                             <div class="carousel-item">
-                                <img src="" alt="New York" width="100%" height="400px">
-                                <div class="carousel-caption">
+                            <img src="<?= URLROOT ?>/img/booking.png" alt=""  width="100%" height="200px">
+                                <div class="carousel-caption text-dark position-relative">
                                     <h3>Music</h3>
                                     <p>A Sweet Melody</p>
                                 </div>
                             </div>
                             <div class="carousel-item">
-                                <img src="" alt="New York" width="100%" height="400px">
-                                <div class="carousel-caption">
+                            <img src="<?= URLROOT ?>/img/booking.png" alt=""  width="100%" height="200px">
+                                <div class="carousel-caption text-dark position-relative">
                                     <h3>Music</h3>
                                     <p>The Best</p>
                                 </div>
                             </div>
                             <div class="carousel-item">
-                                <img src="" alt="New York" width="100%" height="400px">
-                                <div class="carousel-caption">
+                            <img src="<?= URLROOT ?>/img/booking.png" alt=""  width="100%" height="200px">
+                                <div class="carousel-caption text-dark position-relative">
                                     <h3>Los Angeles</h3>
                                     <p>Its For All Ages</p>
                                 </div>
                             </div>
                             <a class="carousel-control-prev" href="#demo" data-slide="prev">
-                                <span class="carousel-control-prev-icon"></span>
+                                <span class="carousel-control-prev-icon bg-darker rounded-circle"></span>
                             </a>
                             <a class="carousel-control-next" href="#demo" data-slide="next">
-                                <span class="carousel-control-next-icon"></span>
+                                <span class="carousel-control-next-icon bg-darker rounded-circle"></span>
                             </a>
                         </div>
+                    </div>
                 </section>
             </div>
         </div>
     </section>
 </section>
 
+</section>
 
 <?php require APPROOT . '/views/inc/pagesfooter.php'; ?>
 
