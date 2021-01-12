bcca1000e5d56f74569b4671dc6c6e0e6190c1d1
Daniel <mabadejedaniel1@gmail.com>
2021-01-11 17:50:59 -0800

    added quicktutorial

---
+++ app/views/pages/quicktutorial.php
@@ -0,0 +1,10 @@
+<?php require APPROOT . '/views/inc/header.php'; ?>
+
+<section>
+    
+</section>
+
+
+<?php require APPROOT . '/views/inc/pagesfooter.php'; ?>
+
+<?php require APPROOT . '/views/inc/footer.php'; ?>
