473ef3b065786caadb25521a5738e084e4eff0d1
Daniel <mabadejedaniel1@gmail.com>
2021-01-19 21:28:21 -0800

    editing edit.php

---
+++ app/views/tasks/edit.php
@@ -0,0 +1,15 @@
+<?php require APPROOT . '/views/inc/header.php'; ?>
+
+
+<div class="position-relative overflow-hidden p-5 m-0  text-center bg-whit landing-bg pt-0 mt-md-0" style="background: linear-gradient(to bottom right, rgb(253, 251, 251), rgba(253, 249, 249, 0)), url('<?= URLROOT; ?>/img/jess-bailey-q10VITrVYUM-unsplash.jpg'); background-size:cover; height:100%;">
+
+    <div class="card mx-auto">
+        <form action="">
+
+        </form>
+    </div>
+</div>
+
+<?php require APPROOT . '/views/inc/pagesfooter.php'; ?>
+
+<?php require APPROOT . '/views/inc/footer.php'; ?>
