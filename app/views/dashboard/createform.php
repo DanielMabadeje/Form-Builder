7cf8e7f125c88cc82a404ed91bb0871d79489ee8
Daniel <mabadejedaniel1@gmail.com>
2021-01-30 22:02:36 -0800

    edited formcreate page

---
+++ app/views/dashboard/createform.php
@@ -0,0 +1,37 @@
+<?php require APPROOT . '/views/dashboard/inc/header.php'; ?>
+
+<?php require APPROOT . '/views/dashboard/inc/sidenav.php'; ?>
+
+<div class="cl ml-auto" id="main">
+    <?php require APPROOT . '/views/dashboard/inc/topnav.php'; ?>
+    <section class="bg-lighter col-12 p-3">
+        <div>
+            <span class="fa fa-chevron-right"></span>Forms
+        </div>
+    </section>
+    <section class="container pt-3">
+        <!-- <h2 class="display-4">Dashboard</h2> -->
+
+        <div class="container">
+            <h2 class="display-4">Select Form Template</h2>
+
+
+
+            <div class="card mt-4">
+                <div class="card-body">
+                    <!-- <div class="card-title mb-0"></div> -->
+                    <div class="table-responsive" style="overflow-x:auto">
+                        <!-- <div class="col-12" style="height: 1px;"></div> -->
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+</div>
+
+
+
+<?php require APPROOT . '/views/dashboard/inc/footer.php'; ?>
