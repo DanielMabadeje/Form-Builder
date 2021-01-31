83d3594f3d117c0fe90434d74411ace4bfdc0a87
Daniel <mabadejedaniel1@gmail.com>
2021-01-30 22:11:37 -0800

    working on todo view

---
+++ app/views/dashboard/todo.php
@@ -0,0 +1,29 @@
+<?php require APPROOT . '/views/dashboard/inc/header.php'; ?>
+
+<?php require APPROOT . '/views/dashboard/inc/sidenav.php'; ?>
+
+<div class="cl ml-auto" id="main">
+    <?php require APPROOT . '/views/dashboard/inc/topnav.php'; ?>
+    <section class="bg-lighter col-12 p-3">
+        <div>
+            <span class="fa fa-chevron-right"></span>Create Form
+        </div>
+    </section>
+    <section class="container pt-3">
+        <!-- <h2 class="display-4">Dashboard</h2> -->
+
+        <div class="container">
+            <h2 class="display-4">Todo List</h2>
+
+
+
+          
+        </div>
+    </section>
+
+
+</div>
+
+
+
+<?php require APPROOT . '/views/dashboard/inc/footer.php'; ?>
