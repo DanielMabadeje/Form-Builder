8bbc3e9480feaf9a28cf96622b8262377582211a
DanielMabadeje <mabadejedaniel1@gmail.com>
2021-04-22 15:34:18 -0700

    editing responseschart page

--- app/views/forms/responseschart.php
+++ app/views/forms/responseschart.php
@@ -59,9 +59,9 @@
             <div class="card-body">
                 <h2>Graphical ResponsesChart Page</h2>
 
-                <pre>
+                <!-- <pre>
                 <?php var_dump($data); ?>
-                </pre>
+                </pre> -->
 
 
 
@@ -140,15 +140,15 @@
             borderWidth: 1
         }]
     };
-    // var options = {
-    //     scales: {
-    //         yAxes: [{
-    //             ticks: {
-    //                 beginAtZero: true
-    //             }
-    //         }]
-    //     }
-    // }
+    var options = {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
 
 
     var myDoughnutChart = new Chart(ctx, {
