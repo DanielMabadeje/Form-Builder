ce08e480065001941bda6283d12ba746dba775df
DanielMabadeje <mabadejedaniel1@gmail.com>
2021-04-22 12:43:10 -0700

    added images

--- app/views/dashboard/createform.php
+++ app/views/dashboard/createform.php
@@ -111,7 +111,7 @@
 
                     <div class="col-md-4 pb-5">
 
-                        <div class="card col-md-12 m-md-2 mt-4 mb-4" style="border-radius:0; height:150px; background:linear-gradient(to right, #00000061, #0000001a), url('<?= URLROOT ?>/img/cake.jpeg'); background-size:cover;">
+                        <div class="card col-md-12 m-md-2 mt-4 mb-4" style="border-radius:0; height:150px; background:linear-gradient(to right, #00000061, #0000001a), url('<?= URLROOT ?>/img/julian-hochgesang-Dkn8-zPIbwo-unsplash.jpg'); background-size:cover;">
                             <div class="card-title mb-0"></div>
                             <div class="card-body text-left mb-5 pb-5">
 
