b18877c1b9677c98e5a46a22b2c528eac9cabd4e
DanielMabadeje <mabadejedaniel1@gmail.com>
2021-04-22 12:42:43 -0700

    added images

--- app/views/dashboard/createform.php
+++ app/views/dashboard/createform.php
@@ -17,128 +17,128 @@
 
 
 
-           <section class="templates">
-            <section class="row">
-                <div class="col-md-4 pb-5">
-                
-                    <div class="card col-md-12 m-md-2 mt-4 mb-4" style="border-radius:0; height:150px; background:linear-gradient(to right, #00000061, #0000001a), url('<?= URLROOT ?>/img/kelly-sikkema-Oz_J_FXKvIs-unsplash.jpg');  background-size:cover;">
-                        <div class="card-title mb-0"></div>
-                        <div class="card-body text-left mb-5 pb-5">                      
+            <section class="templates">
+                <section class="row">
+                    <div class="col-md-4 pb-5">
 
+                        <div class="card col-md-12 m-md-2 mt-4 mb-4" style="border-radius:0; height:150px; background:linear-gradient(to right, #00000061, #0000001a), url('<?= URLROOT ?>/img/kelly-sikkema-Oz_J_FXKvIs-unsplash.jpg');  background-size:cover;">
+                            <div class="card-title mb-0"></div>
+                            <div class="card-body text-left mb-5 pb-5">
+
+                            </div>
+                            <h5 class="mt-5">Blank Form</h5>
                         </div>
-                        <h5 class="mt-5">Blank Form</h5>
                     </div>
-                </div>
-                                
 
-                <div class="col-md-4 pb-5">
-                
-                    <div class="card col-md-12 m-md-2 mt-4 mb-4" style="border-radius:0; height:150px;  background:linear-gradient(to right, #00000061, #0000001a), url('<?= URLROOT ?>/img/katt-yukawa-K0E6E0a0R3A-unsplash.jpg');  background-size:cover;">
-                        <div class="card-title mb-0"></div>
-                        <div class="card-body text-left mb-5 pb-5">                        
 
+                    <div class="col-md-4 pb-5">
+
+                        <div class="card col-md-12 m-md-2 mt-4 mb-4" style="border-radius:0; height:150px;  background:linear-gradient(to right, #00000061, #0000001a), url('<?= URLROOT ?>/img/katt-yukawa-K0E6E0a0R3A-unsplash.jpg');  background-size:cover;">
+                            <div class="card-title mb-0"></div>
+                            <div class="card-body text-left mb-5 pb-5">
+
+                            </div>
+                            <h5 class="mt-5">Donation Form</h5>
                         </div>
-                        <h5 class="mt-5">Donation Form</h5>
                     </div>
-                </div>
 
 
-                <div class="col-md-4 pb-5">
-                
-                    <div class="card col-md-12 m-md-2 mt-4 mb-4" style="border-radius:0; height:150px; background:linear-gradient(to right, #00000061, #0000001a), url('<?= URLROOT ?>/img/sigmund-ZLst8z_M6_8-unsplash.jpg');  background-size:cover;">
-                        <div class="card-title mb-0"></div>
-                        <div class="card-body text-left mb-5 pb-5">
-                            
-                        
+                    <div class="col-md-4 pb-5">
 
+                        <div class="card col-md-12 m-md-2 mt-4 mb-4" style="border-radius:0; height:150px; background:linear-gradient(to right, #00000061, #0000001a), url('<?= URLROOT ?>/img/sigmund-ZLst8z_M6_8-unsplash.jpg');  background-size:cover;">
+                            <div class="card-title mb-0"></div>
+                            <div class="card-body text-left mb-5 pb-5">
+
+
+
+                            </div>
+                            <h5 class="mt-5">RSVP Form</h5>
                         </div>
-                        <h5 class="mt-5">RSVP Form</h5>
+
                     </div>
 
-                </div>
+                    <div class="col-md-4 pb-5">
 
-                <div class="col-md-4 pb-5">
-                
-                    <div class="card col-md-12 m-md-2 mt-4 mb-4" style="border-radius:0; height:150px; background:linear-gradient(to right, #00000061, #0000001a), url('<?= URLROOT; ?>/img/andrew-neel-cckf4TsHAuw-unsplash.jpg'); background-size:cover;">
-                        <div class="card-title mb-0"></div>
-                        <div class="card-body text-left pb-5 mb-5">
-                        
+                        <div class="card col-md-12 m-md-2 mt-4 mb-4" style="border-radius:0; height:150px; background:linear-gradient(to right, #00000061, #0000001a), url('<?= URLROOT; ?>/img/andrew-neel-cckf4TsHAuw-unsplash.jpg'); background-size:cover;">
+                            <div class="card-title mb-0"></div>
+                            <div class="card-body text-left pb-5 mb-5">
 
+
+                            </div>
+                            <h5 class="mt-5">Job Application Form</h5>
                         </div>
-                        <h5 class="mt-5">Job Application Form</h5>
+
                     </div>
 
-                </div>
+                    <div class="col-md-4 pb-5">
 
-                <div class="col-md-4 pb-5">
-                
-                    <div class="card col-md-12 m-md-2 mt-4 mb-4" style="border-radius:0; height:150px; background:linear-gradient(to right, #00000061, #0000001a), url('<?= URLROOT ?>/img/sergey-zolkin-_UeY8aTI6d0-unsplash.jpg'); background-size:cover;">
-                        <div class="card-title mb-0"></div>
-                        <div class="card-body text-left pb-5 mb-5">
-                        
+                        <div class="card col-md-12 m-md-2 mt-4 mb-4" style="border-radius:0; height:150px; background:linear-gradient(to right, #00000061, #0000001a), url('<?= URLROOT ?>/img/sergey-zolkin-_UeY8aTI6d0-unsplash.jpg'); background-size:cover;">
+                            <div class="card-title mb-0"></div>
+                            <div class="card-body text-left pb-5 mb-5">
 
+
+                            </div>
+                            <h5 class="mt-5">Booking Form</h5>
                         </div>
-                        <h5 class="mt-5">Booking Form</h5>
+
                     </div>
 
-                </div>
+                    <div class="col-md-4 pb-5">
 
-                <div class="col-md-4 pb-5">
-                
-                    <div class="card col-md-12 m-md-2 mt-4 mb-4" style="border-radius:0; height:150px; background:linear-gradient(to right, #00000061, #0000001a), url('<?= URLROOT ?>/img/cel-lisboa-60315-unsplash.jpg'); background-size:cover;">
-                        <div class="card-title mb-0"></div>
-                        <div class="card-body text-left pb-5 mb-5">
-                        
+                        <div class="card col-md-12 m-md-2 mt-4 mb-4" style="border-radius:0; height:150px; background:linear-gradient(to right, #00000061, #0000001a), url('<?= URLROOT ?>/img/cel-lisboa-60315-unsplash.jpg'); background-size:cover;">
+                            <div class="card-title mb-0"></div>
+                            <div class="card-body text-left pb-5 mb-5">
 
+
+                            </div>
+                            <h5 class="mt-5">Food Form</h5>
                         </div>
-                        <h5 class="mt-5">Food Form</h5>
                     </div>
-                </div>
 
-                <div class="col-md-4 pb-5">
-                
-                    <div class="card col-md-12 m-md-2 mt-4 mb-4" style="border-radius:0; height:150px; background:linear-gradient(to right, #00000061, #0000001a), url('<?= URLROOT ?>/img/cake.jpeg'); background-size:cover;">
-                        <div class="card-title mb-0"></div>
-                        <div class="card-body text-left mb-5 pb-5">
-                        
+                    <div class="col-md-4 pb-5">
 
+                        <div class="card col-md-12 m-md-2 mt-4 mb-4" style="border-radius:0; height:150px; background:linear-gradient(to right, #00000061, #0000001a), url('<?= URLROOT ?>/img/cake.jpeg'); background-size:cover;">
+                            <div class="card-title mb-0"></div>
+                            <div class="card-body text-left mb-5 pb-5">
+
+
+                            </div>
+                            <h5 class="mt-5">Cake Order Form</h5>
                         </div>
-                        <h5 class="mt-5">Cake Order Form</h5>
                     </div>
-                </div>
 
 
 
-            <div class="col-md-4 pb-5">
-                
-                <div class="card col-md-12 m-md-2 mt-4 mb-4" style="border-radius:0; height:150px; background:linear-gradient(to right, #00000061, #0000001a), url('<?= URLROOT ?>/img/cake.jpeg'); background-size:cover;">
-                    <div class="card-title mb-0"></div>
-                    <div class="card-body text-left mb-5 pb-5">
-                    
+                    <div class="col-md-4 pb-5">
 
+                        <div class="card col-md-12 m-md-2 mt-4 mb-4" style="border-radius:0; height:150px; background:linear-gradient(to right, #00000061, #0000001a), url('<?= URLROOT ?>/img/cake.jpeg'); background-size:cover;">
+                            <div class="card-title mb-0"></div>
+                            <div class="card-body text-left mb-5 pb-5">
+
+
+                            </div>
+                            <h5 class="mt-5">Contact Form</h5>
+                        </div>
                     </div>
-                    <h5 class="mt-5">Contact Form</h5>
-                </div>
-            </div>
 
 
 
 
-            <div class="col-md-4 pb-5">
-                
-                <div class="card col-md-12 m-md-2 mt-4 mb-4" style="border-radius:0; height:150px; background:linear-gradient(to right, #00000061, #0000001a), url('<?= URLROOT ?>/img/cake.jpeg'); background-size:cover;">
-                    <div class="card-title mb-0"></div>
-                    <div class="card-body text-left mb-5 pb-5">
-                    
+                    <div class="col-md-4 pb-5">
 
+                        <div class="card col-md-12 m-md-2 mt-4 mb-4" style="border-radius:0; height:150px; background:linear-gradient(to right, #00000061, #0000001a), url('<?= URLROOT ?>/img/isaac-smith-AT77Q0Njnt0-unsplash.jpg'); background-size:cover;">
+                            <div class="card-title mb-0"></div>
+                            <div class="card-body text-left mb-5 pb-5">
+
+
+                            </div>
+                            <h5 class="mt-5">Survey Form</h5>
+                        </div>
                     </div>
-                    <h5 class="mt-5">Survey Form</h5>
-                </div>
-            </div>
 
 
+                </section>
             </section>
-           </section>
         </div>
     </section>
 
