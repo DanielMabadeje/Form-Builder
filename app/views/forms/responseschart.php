e383903d682501b6664099ce39513fcc73edfa6d
DanielMabadeje <mabadejedaniel1@gmail.com>
2021-04-24 15:05:33 -0700

    editing bar chart

--- app/views/forms/responseschart.php
+++ app/views/forms/responseschart.php
@@ -131,8 +131,7 @@
 
         } else if (currentIndex=checkifDropdown(formarray.questions[index].question_id)) {
             var labels = ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'];
-            console.log(formarray.questions[index].options);
-            // var labels = formarray.questions[index].options;
+            // console.log(formarray.questions[index].options);
             var labels=convertObjectToArray(formarray.questions[index].options)
             var data = {
                 labels: labels,
@@ -141,21 +140,9 @@
                     data: [65, 59, 80, 81, 56, 55, 40],
                     backgroundColor: [
                         'rgba(255, 99, 132, 0.2)',
-                        // 'rgba(255, 159, 64, 0.2)',
-                        // 'rgba(255, 205, 86, 0.2)',
-                        // 'rgba(75, 192, 192, 0.2)',
-                        // 'rgba(54, 162, 235, 0.2)',
-                        // 'rgba(153, 102, 255, 0.2)',
-                        // 'rgba(201, 203, 207, 0.2)'
                     ],
                     borderColor: [
                         'rgb(255, 99, 132)',
-                        // 'rgb(255, 159, 64)',
-                        // 'rgb(255, 205, 86)',
-                        // 'rgb(75, 192, 192)',
-                        // 'rgb(54, 162, 235)',
-                        // 'rgb(153, 102, 255)',
-                        // 'rgb(201, 203, 207)'
                     ],
                     borderWidth: 1
                 }]
