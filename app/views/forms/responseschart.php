70da67daa2793b2942a3208f3ac54a823d4d7cfe
DanielMabadeje <mabadejedaniel1@gmail.com>
2021-04-24 14:55:57 -0700

    working on bar chart

--- app/views/forms/responseschart.php
+++ app/views/forms/responseschart.php
@@ -130,8 +130,8 @@
 
 
         } else if (currentIndex=checkifDropdown(formarray.questions[index].question_id)) {
-            console.log(currentIndex);
             var labels = ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'];
+            // var labels = formarray.questions[index].options;
             var data = {
                 labels: labels,
                 datasets: [{
@@ -139,21 +139,21 @@
                     data: [65, 59, 80, 81, 56, 55, 40],
                     backgroundColor: [
                         'rgba(255, 99, 132, 0.2)',
-                        'rgba(255, 159, 64, 0.2)',
-                        'rgba(255, 205, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(201, 203, 207, 0.2)'
+                        // 'rgba(255, 159, 64, 0.2)',
+                        // 'rgba(255, 205, 86, 0.2)',
+                        // 'rgba(75, 192, 192, 0.2)',
+                        // 'rgba(54, 162, 235, 0.2)',
+                        // 'rgba(153, 102, 255, 0.2)',
+                        // 'rgba(201, 203, 207, 0.2)'
                     ],
                     borderColor: [
                         'rgb(255, 99, 132)',
-                        'rgb(255, 159, 64)',
-                        'rgb(255, 205, 86)',
-                        'rgb(75, 192, 192)',
-                        'rgb(54, 162, 235)',
-                        'rgb(153, 102, 255)',
-                        'rgb(201, 203, 207)'
+                        // 'rgb(255, 159, 64)',
+                        // 'rgb(255, 205, 86)',
+                        // 'rgb(75, 192, 192)',
+                        // 'rgb(54, 162, 235)',
+                        // 'rgb(153, 102, 255)',
+                        // 'rgb(201, 203, 207)'
                     ],
                     borderWidth: 1
                 }]
