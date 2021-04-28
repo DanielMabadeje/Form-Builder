984041e565d603742baece5b99fe876958aa4e6f
DanielMabadeje <mabadejedaniel1@gmail.com>
2021-04-28 11:52:39 -0700

    adding links

--- app/views/dashboard/createform.php
+++ app/views/dashboard/createform.php
@@ -29,7 +29,7 @@
                             <h5 class="mt-5">Blank Form</h5>
                         </div>
                     </a>
-                    <a class="col-md-4 pb-5">
+                    <a href="<?= URLROOT; ?>/forms/add/donation" class="col-md-4 pb-5">
 
                         <div class="card col-md-12 m-md-2 mt-4 mb-4" style="border-radius:0; height:150px;  background:linear-gradient(to right, #00000061, #0000001a), url('<?= URLROOT ?>/img/katt-yukawa-K0E6E0a0R3A-unsplash.jpg');  background-size:cover;">
                             <div class="card-title mb-0"></div>
