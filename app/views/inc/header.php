583783a69d00577877c0cee1764135ed5fe59438
DanielMabadeje <mabadejedaniel1@gmail.com>
2021-04-30 12:30:54 -0700

    changing fonts

--- app/views/inc/header.php
+++ app/views/inc/header.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/style.css">
     <title><?php echo SITENAME; ?></title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -13,7 +13,6 @@
 
 
     <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Nunito+Sans%3A300%2C400%2C600%2C700%2C300italic%2C400italic%2C600italic%2C700italic%7CQuicksand%3A500&amp;ver=1.0.0#038;subset">
-    <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/style.css">
 
 
     <!-- script config -->
