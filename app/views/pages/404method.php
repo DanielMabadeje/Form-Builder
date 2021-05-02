e1bcd9c1b7f79266d4c2ddd64ccdda42b57e0264
DanielMabadeje <mabadejedaniel1@gmail.com>
2021-05-02 10:18:56 -0700

    edited 404 method not found

---
+++ app/views/pages/404method.php
@@ -0,0 +1,13 @@
+<?php require APPROOT . '/views/inc/header.php'; ?>
+<section class="col-12">
+    <section class="mx-auto">
+        <section class="pt-5 mt-5">
+            <h1 class="display-4">404 | METHOD NOT FOUND</h1>
+            
+        </section>
+
+      
+    </section>
+</section>
+
+<?php require APPROOT . '/views/inc/footer.php' ?>
