d541c1dd9d450d2088760ac63ef71c3c20c140d4
DanielMabadeje <mabadejedaniel1@gmail.com>
2021-05-02 10:26:25 -0700

    added text-center

--- app/views/pages/comingsoon.php
+++ app/views/pages/comingsoon.php
@@ -6,13 +6,13 @@
             <h3>ARE YOU READY?</h3>
         </section>
 
-        <section class="pt-5">
+        <section class="pt-5 mx-auto col-md-4">
             <form class="form-inline">
                 <div class="form-group mb-2">
                     <label for="staticEmail2" class="sr-only">Email</label>
                     <input type="text" readonly class="form-control-plaintext" id="staticEmail2" value="email@example.com">
                 </div>
-                <button type="submit" class="btn btn-primary mb-2">Confirm identity</button>
+                <button type="submit" class="btn bg-main text-white mb-2">Confirm identity</button>
             </form>
         </section>
     </section>
