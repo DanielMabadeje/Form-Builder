afd8be2da407e769827ab02b75066175cd498798
DanielMabadeje <mabadejedaniel1@gmail.com>
2021-05-02 12:06:24 -0700

    adding letterspacing

--- app/views/pages/comingsoon.php
+++ app/views/pages/comingsoon.php
@@ -1,19 +1,21 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
-<section class="col-12">
+<section class="col-md-12">
     <section class="mx-auto text-center">
         <section class="pt-5 mt-5">
-            <h1 class="display-4">COMING SOON</h1>
+            <h1 class="display-4 lt-2">COMING <span class="ml-md-4">SOON</span> </h1>
             <h3>ARE YOU READY?</h3>
         </section>
 
-        <section class="pt-5 mx-auto col-md-4">
-            <form class="form-inline">
-                <div class="form-group mb-2">
-                    <label for="staticEmail2" class="sr-only">Email</label>
-                    <input type="text" readonly class="form-control-plaintext" id="staticEmail2" value="email@example.com">
-                </div>
-                <button type="submit" class="btn bg-main text-white mb-2">Confirm identity</button>
-            </form>
+        <section class="pt-5 ">
+            <div class="mx-auto col-md-4">
+                <form class="form-inline col-md-12">
+                    <div class="form-group mb-2 col-md-8" style="border:2px solid #5631af;">
+                        <label for="staticEmail2" class="sr-only">Email</label>
+                        <input type="text" placeholder="Your Email here...." class="form-control-plaintext" id="staticEmail2" value="">
+                    </div>
+                    <button type="submit" class="btn bg-main col-md-4  text-white p-2 mb-2" style="border-radius:0 !important;">Notify Me</button>
+                </form>
+            </div>
         </section>
     </section>
 </section>
