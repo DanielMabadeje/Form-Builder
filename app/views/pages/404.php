e033d25cc8860a71d78dfa74511014aba81dead6
DanielMabadeje <mabadejedaniel1@gmail.com>
2021-05-02 10:22:34 -0700

    added text-center

--- app/views/pages/404.php
+++ app/views/pages/404.php
@@ -1,6 +1,6 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
 <section class="col-12">
-    <section class="mx-auto">
+    <section class="mx-auto text-center">
         <section class="pt-5 mt-5">
             <h1 class="display-4">404 | NOT FOUND</h1>
             
