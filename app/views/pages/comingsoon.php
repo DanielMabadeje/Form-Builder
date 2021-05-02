92966ebc9aa743791667aef4aa457c1ddef12273
DanielMabadeje <mabadejedaniel1@gmail.com>
2021-05-02 10:06:41 -0700

    editing coming soon page

--- app/views/pages/comingsoon.php
+++ app/views/pages/comingsoon.php
@@ -1,4 +1,15 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
-<section></section>
+<section>
+    <section class="mx-auto">
+        <section class="pt-5 mt-5">
+            <h1 class="display-4">COMING SOON</h1>
+            <h3>ARE YOU READY?</h3>
+        </section>
+
+        <section class="pt-5">
+            <form action=""></form>
+        </section>
+    </section>
+</section>
 
 <?php require APPROOT . 'views/inc/footer.php' ?>
