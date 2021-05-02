35e2ba31f146bc7c648dcc5e16ee083dba5aed62
DanielMabadeje <mabadejedaniel1@gmail.com>
2021-05-02 10:22:20 -0700

    added text-center

--- app/views/pages/comingsoon.php
+++ app/views/pages/comingsoon.php
@@ -1,6 +1,6 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
 <section class="col-12">
-    <section class="mx-auto">
+    <section class="mx-auto text-center">
         <section class="pt-5 mt-5">
             <h1 class="display-4">COMING SOON</h1>
             <h3>ARE YOU READY?</h3>
