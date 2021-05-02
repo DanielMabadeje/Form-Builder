5c59b08449e059524b277920c42bd372905c8270
DanielMabadeje <mabadejedaniel1@gmail.com>
2021-05-02 12:25:36 -0700

    editing contact page

--- app/views/pages/contact.php
+++ app/views/pages/contact.php
@@ -1,7 +1,7 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
 
 
-<div class="position-relative overflow-hidden p-5 m-0  text-center bg-whit landing-bg pt-0 mt-md-0" style="background: linear-gradient(to bottom right, rgb(253, 251, 251), rgba(253, 249, 249, 0)), url('<?= URLROOT; ?>/img/jess-bailey-q10VITrVYUM-unsplash.jpg'); background-size:cover; height:100%;">
+<div class="position-relative overflow-hidden p-5 m-0  text-center bg-whit landing-bg pt-0 mt-md-0" style="background: linear-gradient(to bottom right, rgb(253, 251, 251, 0.98), rgba(253, 249, 249, 0.97)), url('<?= URLROOT; ?>/img/julian-hochgesang-Dkn8-zPIbwo-unsplash.jpg'); background-size:cover; height:100%;">
     <div class="col-md-7 p-lg-5 my-5 text-dark mx-auto">
         <h1 class="display-4 font-weight-normal">Contact Us</h1>
     </div>
