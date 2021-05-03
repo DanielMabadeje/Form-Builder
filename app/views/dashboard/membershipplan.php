a8b116651faee2b5e33ea351ac97f531cc3e9658
DanielMabadeje <mabadejedaniel1@gmail.com>
2021-05-03 06:13:49 -0700

    editing membership plan settings

--- app/views/dashboard/membershipplan.php
+++ app/views/dashboard/membershipplan.php
@@ -19,21 +19,21 @@
 
             <div class="container">
                 <div class="row text-center">
-                    <div class="card col text-center m-2">
+                    <div class="card col text-center m-2 border-0 custom-shadow">
                         <h2 class="display-4">Free</h2>
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum ipsa blanditiis, ex molestiae ad voluptates!</p>
                         <h4>$0.00 per month</h4>
 
                         <div>
                             <p>Accepting Of Payments</p>
-                            <p>Accepting Of Payments</p>
-                            <p>Accepting Of Payments</p>
+                            <p>Maximum of 10 Forms</p>
+                            <p>Maximum 3000 Responses</p>
                             <p>Accepting Of Payments</p>
                         </div>
 
                         <!-- <a href="" class="btn btn-primary p-3 col-md-8 mx-auto m-3">Sign Up For Free</a> -->
                     </div>
-                    <div class="card col text-center m-2">
+                    <div class="card col text-center m-2 border-0 custom-shadow">
                         <h2 class="display-4">Classic</h2>
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum ipsa blanditiis, ex molestiae ad voluptates!</p>
                         <h4>$40.00 per month</h3>
@@ -46,20 +46,20 @@
                                 <p>Accepting Of Payments</p>
                             </div>
 
-                            <a href="" class="btn btn-primary p-3 col-md-8 mx-auto m-3">Sign Up For Classic</a>
+                            <a href="" class="btn btn-primary p-3 col-md-8 mx-auto m-3">Switch To Classic</a>
                     </div>
-                    <div class="card col text-center m-2">
+                    <div class="card col text-center m-2 border-0 custom-shadow">
                         <h2 class="display-4">Premium</h2>
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum ipsa blanditiis, ex molestiae ad voluptates!</p>
                         <h4>$70.00 per month</h3>
                             <div>
                                 <p>Accepting Of Payments</p>
-                                <p>Accepting Of Payments</p>
-                                <p>Accepting Of Payments</p>
-                                <p>Accepting Of Payments</p>
-                                <p>Accepting Of Payments</p>
+                                <p>Unlimited Responses</p>
+                                <p>Versatile Customization</p>
+                                <p>Custom Domain</p>
+                                <p>Form Fo Teams</p>
                             </div>
-                            <a href="" class="btn btn-primary p-3 col-md-8 mx-auto m-3">Sign Up For Premium</a>
+                            <a href="" class="btn btn-primary p-3 col-md-8 mx-auto m-3">Switch To Premium</a>
                     </div>
                 </div>
             </div>
