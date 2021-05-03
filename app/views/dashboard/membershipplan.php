00119e74a6671f1287b3b10bd49edc9b9e335447
DanielMabadeje <mabadejedaniel1@gmail.com>
2021-05-03 06:30:31 -0700

    editing membership plan

--- app/views/dashboard/membershipplan.php
+++ app/views/dashboard/membershipplan.php
@@ -12,54 +12,61 @@
     <section class="container pt-3">
         <!-- <h2 class="display-4">Dashboard</h2> -->
 
-        <div class="container">
+        <div class="container mt-5 pt-5">
             <h2 class="display-4">Membership Plan</h2>
 
 
 
-            <div class="container">
+            <div class="con">
                 <div class="row text-center">
-                    <div class="card col text-center m-2 border-0 custom-shadow">
-                        <h2 class="display-4">Free</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum ipsa blanditiis, ex molestiae ad voluptates!</p>
-                        <h4>$0.00 per month</h4>
+                    <div class="col-md-4">
+                        <div class="card col-12 text-center m-2 border-0 custom-shadow">
+                            <h2 class="display-4">Free</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum ipsa blanditiis, ex molestiae ad voluptates!</p>
+                            <h4>$0.00 per month</h4>
 
-                        <div>
-                            <p>Accepting Of Payments</p>
-                            <p>Maximum of 10 Forms</p>
-                            <p>Maximum 3000 Responses</p>
-                            <p>Accepting Of Payments</p>
+                            <div>
+                                <p>Accepting Of Payments</p>
+                                <p>Maximum of 10 Forms</p>
+                                <p>Maximum 3000 Responses</p>
+                                <p>Accepting Of Payments</p>
+                            </div>
+
+                            <!-- <a href="" class="btn btn-primary p-3 col-md-8 mx-auto m-3">Sign Up For Free</a> -->
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card col-12 text-center m-2 border-0 custom-shadow">
+                            <h2 class="display-4">Classic</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum ipsa blanditiis, ex molestiae ad voluptates!</p>
+                            <h4>$40.00 per month</h3>
+
+                                <div>
+                                    <p>Accepting Of Payments</p>
+                                    <p>Accepting Of Payments</p>
+                                    <p>Accepting Of Payments</p>
+                                    <p>Accepting Of Payments</p>
+                                    <p>Accepting Of Payments</p>
+                                </div>
+
+                                <a href="" class="btn text-white bg-main border-color-main p-3 col-md-8 mx-auto m-3">Switch To Classic</a>
                         </div>
 
-                        <!-- <a href="" class="btn btn-primary p-3 col-md-8 mx-auto m-3">Sign Up For Free</a> -->
                     </div>
-                    <div class="card col text-center m-2 border-0 custom-shadow">
-                        <h2 class="display-4">Classic</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum ipsa blanditiis, ex molestiae ad voluptates!</p>
-                        <h4>$40.00 per month</h3>
-
-                            <div>
-                                <p>Accepting Of Payments</p>
-                                <p>Accepting Of Payments</p>
-                                <p>Accepting Of Payments</p>
-                                <p>Accepting Of Payments</p>
-                                <p>Accepting Of Payments</p>
-                            </div>
-
-                            <a href="" class="btn btn-primary p-3 col-md-8 mx-auto m-3">Switch To Classic</a>
-                    </div>
-                    <div class="card col text-center m-2 border-0 custom-shadow">
-                        <h2 class="display-4">Premium</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum ipsa blanditiis, ex molestiae ad voluptates!</p>
-                        <h4>$70.00 per month</h3>
-                            <div>
-                                <p>Accepting Of Payments</p>
-                                <p>Unlimited Responses</p>
-                                <p>Versatile Customization</p>
-                                <p>Custom Domain</p>
-                                <p>Form Fo Teams</p>
-                            </div>
-                            <a href="" class="btn btn-primary p-3 col-md-8 mx-auto m-3">Switch To Premium</a>
+                    <div class="col-md-4">
+                        <div class="card col-12 text-center m-2 border-0 custom-shadow">
+                            <h2 class="display-4">Premium</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum ipsa blanditiis, ex molestiae ad voluptates!</p>
+                            <h4>$70.00 per month</h3>
+                                <div>
+                                    <p>Accepting Of Payments</p>
+                                    <p>Unlimited Responses</p>
+                                    <p>Versatile Customization</p>
+                                    <p>Custom Domain</p>
+                                    <p>Form Fo Teams</p>
+                                </div>
+                                <a href="" class="btn text-white bg-main border-color-main p-3 col-md-8 mx-auto m-3">Switch To Premium</a>
+                        </div>
                     </div>
                 </div>
             </div>
