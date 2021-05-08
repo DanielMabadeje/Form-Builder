c39d4cbb96a2647126df79e8d774fed614b4b718
DanielMabadeje <mabadejedaniel1@gmail.com>
2021-05-08 12:57:17 -0700

    added another image

--- app/views/forms/edit.php
+++ app/views/forms/edit.php
@@ -136,7 +136,7 @@
                 <h2>Share Your Amazing Form</h2>
 
                 <div class="col">
-                    <input type="text" class="col" value="<?= URLROOT; ?>/forms/views/<?= $data->form_id ?>">
+                    <input type="text" class="col input-group-text col-12 text-left p-1 mt-4" value="<?= URLROOT; ?>/forms/views/<?= $data->form_id ?>">
 
                 </div>
             </div>
